5a481aefe287bb151a88e4363199699d382ff5a3
Abhay Maurya <abhaymaurya452002@gmail.com>
2023-02-18 20:47:06 +0000

    Connected API for dynamic use

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
-        <div className='flex item-center justify-center my-6'>
-            <p className='text-white text-xl font-extralight'>
-                Tuesday, 20 February 2023 | Local time: 2:20 PM
-            </p>
-        </div>
-        <div className='flex items-center justify-center my-3'>
-            <p className='text-white text-3xl font-medium'>
-                Berlin, DE
-            </p>
-        </div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          {formatToLocalTime(dt, timezone)}
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">{`${name}, ${country}`}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
